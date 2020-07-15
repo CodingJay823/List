7931c52b4af443c656ff99d6bd3e0ff8f36f4b6b
CodingJay823 <67976024+CodingJay823@users.noreply.github.com>
2020-07-15 14:36:40 -0500

    Exercise Complete

---
+++ Jada_H_OperatingExercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Variable definition: defiining the numbers being calculated
+var num1 = 10
+var num2 = 5
+
+// Defining of the operations: +, -, *, /
+let add = num1 + num2
+let subtract = num1 - num2
+let multiply = num1 * num2
+let divide = num1 / num2
+
+// Printing the results of the operations; operation performance
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
